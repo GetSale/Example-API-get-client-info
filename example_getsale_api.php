e6d36c0ec7d12c50ee96ba783a4e9bc8a93cef3c
Developer <chernozor@yandex.ru>
2017-02-15 10:04:45 +0300

    Update example_getsale_api.php

--- example_getsale_api.php
+++ example_getsale_api.php
@@ -1,35 +1,59 @@
 <?php
 //Пример работы GetSale API для получения данных о пользователях
 
-//Получение контента от GetSale
+//Укажите свой ключ API
+define('G_APIKEY', 'your_API_KEY');
+
+//Получаем контент от GetSale
 $requestBody = file_get_contents('php://input');
 
+//Проверяем подлинность полученного запросa
+//Логирование хэдеров
+foreach (getallheaders() as $name => $value) {
+    if ($name == "X-GetSale-Signature") {
+        $newSigna = hash("sha256", "$requestBody" . G_APIKEY);
+
+        if (strcmp($newSigna, $value) == 0) {
+            //Пишем в log
+            $file = 'log.txt';
+            $current = file_get_contents($file);
+            $current .= "getSigna: " . $value . "\n";
+            $current .= "mySigna: " . $newSigna . "\n";
+            file_put_contents($file, $current);
+        } else {
+            //Пишем в log
+            $file = 'log.txt';
+            $current = file_get_contents($file);
+            $current .= "getSigna: " . $value . "\n";
+            $current .= "mySigna: " . $newSigna . "\n";
+            $current .= "error: Signature verification failed. \n";
+            file_put_contents($file, $current);
+            exit;
+        }
+    }
+}
+
+//Продолжаем, если запрос полученный от GetSale правильный
 $decodedBody = json_decode($requestBody, true);
 $fields = $decodedBody['fields'];
 
-//Получаем данные
 $eventEmail = $fields['email'];
 $eventFirstName = $fields['firstName'];
 $widget_id = $decodedBody['widget_id'];
+$eventUrl = $decodedBody['url'];
 
-//TO_DO Добавить поле url
-
-//Сообщаем об успешном получении, отвечаем 200OK
+//Сообщаем об успешном получении, сообщаем 200OK
 http_response_code(200);
 
-//Фильтр на неинтересующие нас запросы
+//Фильтр на неинтересующие наз запросы
 if (empty($widget_id)) {
     exit;
 }
 
-//Продолжаем, если запрос от GetSale получен
-//TO_DO Проверяем подлинность полученного запроса
-
-//Используем полученые данные от GetSale
-//Например, запишем их в log
+//Логирование полученных данных от GetSale
 $file = 'log.txt';
 $current = file_get_contents($file);
 $current .= 'time: ' . date('Y-m-d h:i:s A') . '\n';
-$current .= "fields: eventEmail: $eventEmail, eventFirstName: $eventFirstName, widget_id: $widget_id. \n";
+$current .= "fields: eventEmail:$eventEmail,  eventPhone:$eventPhone, eventFirstName: $eventFirstName, eventLastName: $eventLastName, eventOther: $eventOther, eventUrl: $eventUrl, widget_id: $widget_id \n";
 file_put_contents($file, $current);
 ?>
